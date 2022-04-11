34d929d82eb2675d811616011410eb60931267fa
gustavo-gabriel-OL <gustavogbriel2004@gmail.com>
2022-04-11 16:05:58 -0300

    🤯

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
-echo "Seja muito bem vindo, não ligue se está vazio, estamos numa página de tarefa de sábado letivo, as coisas costumam ser paradas assim mesmo kk  😅 ";
+include("conexao.php");
+
+echo "Seja muito bem vindo, não ligue se estiver vazio, estamos numa página de tarefa de sábado letivo, as coisas costumam ser paradas assim mesmo kk  😅";
